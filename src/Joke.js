0d76bc232ccf1d3cf69965a0c0f494b89863a7b1
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-10 16:07:06 +0100

    created a function to getColor of the votes radius

--- src/Joke.js
+++ src/Joke.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react'
 import './Joke.css'
 
 export class Joke extends Component {
+
+  getColor() {
+    if (this.props.votes >= 15) {
+      return "#4CAF50";
+    } else if (this.props.votes >= 12) {
+      return "#8BC34A";
+    } else if (this.props.votes >= 9) {
+      return "#CDDC39";
+    } else if (this.props.votes >= 6) {
+      return "#FFEB3B";
+    } else if (this.props.votes >= 3) {
+      return "#FFC107";
+    } else if (this.props.votes >= 0) {
+      return "#FF9800";
+    } else {
+      return "#f44336";
+    }
+  }
+
   render() {
     return (
       <div className='Joke'>
